b2daa9ab99fca3cb8bb077247d0bcf5f1046cf31
Spencer Ferris <3319370+spencewenski@users.noreply.github.com>
2026-02-27 10:03:32 -0800

    wip

--- pgmq-rs/src/bin/pgmq-cli.rs
+++ pgmq-rs/src/bin/pgmq-cli.rs
@@ -6,8 +6,6 @@ use std::process;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    panic!("Foo");
-    log::error!("log1");
     let matches = Command::new("pgmq-cli")
         .about("PGMQ CLI tool for installing and managing PostgreSQL message queues")
         .subcommand(
@@ -21,19 +19,18 @@ async fn main() {
                 ),
         )
         .get_matches();
-    log::error!("log2: {:?}", matches);
 
     match matches.subcommand() {
         Some(("install", sub_matches)) => {
-            let database_url = sub_matches.get_one::<String>("database_url").unwrap();
-            log::error!("log2: {}", database_url);
+            let database_url = sub_matches
+                .get_one::<String>("database_url")
+                .expect("Unable to fetch db url from args");
 
             let pool = PgPool::connect(database_url)
                 .await
                 .expect("Failed to connect to database");
 
             if let Err(e) = install_sql(&pool).await {
-                log::error!("Error installing PGMQ: {}", e);
                 process::exit(1);
             }
         }
